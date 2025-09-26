e95aa40b923320516982943ad40e2e63dfafa6f8
Sofya175 <zueva-sv@mail.ru>
2025-09-27 02:55:13 +0300

    + upr3

--- upr2.py
+++ upr2.py
@@ -4,14 +4,15 @@
 #                       "нормальных" (от 10 до 21)
 #                       и "жарких" (остальных) значений
 
-
-
 hot_counter = 0
 norm_counter  = 0
 cold_counter= 0
 
 for i in range(12):
     temp_air = int(input('введите температуру воздуха '))
+    if temp_air < -273:
+        print('ERROR')
+        break          #не о чем больше с таким пользователем разговаривать, прекращаем цикл
 
     if temp_air < 10:
         cold_counter += 1
@@ -20,4 +21,4 @@ for i in range(12):
     else:
         hot_counter += 1
 
-print(f"прохладных {cold_counter}")
+print(f'холодных: {cold_counter}, нормальных: {norm_counter}, жарких: {hot_counter}')

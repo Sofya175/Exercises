18f70f067b85112bf48959369ea74a355e9f379d
Sofya175 <zueva-sv@mail.ru>
2025-09-26 16:31:31 +0300

    Initial commit

---
+++ upr2.py
@@ -0,0 +1,23 @@
+
+# пользователь 12 раз вводит числа - температуру воздуха
+# Подсчитать количество "прохладных" (менее 10)
+#                       "нормальных" (от 10 до 21)
+#                       и "жарких" (остальных) значений
+
+
+
+hot_counter = 0
+norm_counter  = 0
+cold_counter= 0
+
+for i in range(12):
+    temp_air = int(input('введите температуру воздуха '))
+    
+    if temp_air < 10:
+        cold_counter += 1
+    elif 10 <= temp_air <= 21:
+        norm_counter +=1
+    else:
+        hot_counter += 1
+
+print(f"прохладных {cold_counter}")
